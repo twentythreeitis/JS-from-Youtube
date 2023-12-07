f13156e7d426e4decb76dc3b3d8d35f4e2836802
Aryan Vashith <118191817+twentythreeitis@users.noreply.github.com>
2023-12-07 08:42:36 +0000

    Datatype summary

---
+++ 01_basics/dtypes-summary.js
@@ -0,0 +1,10 @@
+//Primitive
+
+//7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//JavaScript is static or dynamic 
+
+
+//Reference(Non-primitive)
+
+// Array, Objects, Functions
