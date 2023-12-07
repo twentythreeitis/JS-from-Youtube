d84edf98ecde9e5d7d94d84fa6bb05d9933dcf70
Aryan Vashith <118191817+twentythreeitis@users.noreply.github.com>
2023-12-07 07:01:00 +0000

    Operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,16 @@
+let str1 = "hello"
+let str2 = " aryan"
+
+let str3 = str1 + str2
+console.log(str3);  //hello aryan
+
+console.log("1" + 2);     //12
+console.log(1+ "2");      //12
+console.log("1" + 2 +2);       //122 because string first then whole string
+console.log(1 + 2 + "2");      //32 string will get concatinated at the last after addition.
+
+
+console.log(true);   //true
+console.log(+true);   //1
+console.log(+"");    //0
+ 
