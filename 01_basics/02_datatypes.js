ab34167e94f3c0cc35a072af1312eef105f71a88
Aryan Vashith <118191817+twentythreeitis@users.noreply.github.com>
2023-12-07 04:57:05 +0000

    Data Types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  /*to treat all JS code with respect to newer version. 
+                Once done, we can't undo it by reusing again in the same code*/
+
+//alert(3+4);  alert is a fuction of document (HTML) but here we cant use it in Node environment)
+
+//DATA TYPES//
+
+let name = "Aryan"
+let age = 22
+let isLoggedIn = false
+
+
+//number => 2 to the power 53 range
+//bigint
+//string => ""
+//boolean => true/false
+//null => stadalone value
+//undefined => value not assigned
+//symbol => uniqueness
+
+
+//objects
+console.log(typeof null); //Object
+console.log(typeof undefined);  //undefined
