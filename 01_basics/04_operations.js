300a18891b1ee02c1d85bd26c5d6a1977381a496
Aryan Vashith <118191817+twentythreeitis@users.noreply.github.com>
2023-12-07 08:06:55 +0000

    Comparison

--- 01_basics/04_operations.js
+++ 01_basics/04_operations.js
@@ -14,3 +14,25 @@ console.log(true);   //true
 console.log(+true);   //1
 console.log(+"");    //0
  
+/*--------COMPARISON---------*/
+
+console.log("2" > 1);    //true
+console.log("02" > 1);   //true
+
+console.log(null > 0);    //false  here null is converted to 0=> 0>0 false
+console.log(null ==0);    //false  here no covnersion because ==
+console.log(null >= 0);    //true  here null=> 0, therefore 0 >= 0 true
+
+/*The reason is that a equality check == ad comparison >,<,>=,<=
+work differently.
+Comparisons convert null to a number, treating it as 0.
+Therefore, null >= 0 is true and null > 0 is false.
+*/
+
+console.log(undefined == 0);  //false
+console.log(undefined > 0);   //false
+console.log(null == undefined);   //true   
+console.log(null >= undefined);   //false
+console.log(null <= undefined);   //false
+console.log(NaN == undefined);   //false
+
