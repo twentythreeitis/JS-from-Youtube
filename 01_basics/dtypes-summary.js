e35305e41c1ca70dca517c3abd504768d011ffc8
Aryan Vashith <118191817+twentythreeitis@users.noreply.github.com>
2023-12-07 10:27:22 +0000

    Datatypes-overview

--- 01_basics/dtypes-summary.js
+++ 01_basics/dtypes-summary.js
@@ -2,9 +2,35 @@
 
 //7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-//JavaScript is static or dynamic 
+/*JavaScript is dynamically typed laguage because the data types of variables
+are determined by the value they hold at runtime and can change throughout 
+the program*/
 
 
 //Reference(Non-primitive)
 
 // Array, Objects, Functions
+
+//Array
+const heros = ["Shaktiman", "Naagraj", "Doga"]; 
+
+//Object
+let myObj = {
+    name : "Aryan",
+    age : 22
+}
+
+//Function
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+/*-----------------------------------------------------*/
+console.log(typeof(myObj));  //Object
+console.log(typeof(myFunction));   //Function Object
+console.log(typeof(heros));   //Object
+/*-----------------------------------------------------/
+
+/*Jitnebhi reference data types h unka typeof "OBJECT" hi hota h except 
+functions ka.. unka typeof "FUNCTIONS OBJECT" hota.*/
+
