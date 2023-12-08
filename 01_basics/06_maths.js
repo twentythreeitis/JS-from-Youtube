784923e1bb9046b11832c5d3ca513ee88efa481d
Aryan Vashith <118191817+twentythreeitis@users.noreply.github.com>
2023-12-08 08:57:04 +0000

    Math.random

--- 01_basics/06_maths.js
+++ 01_basics/06_maths.js
@@ -12,3 +12,25 @@ console.log(balance.toFixed(2));   //100.00   brings upto 2 decimal places.
 const otherNumber = 23.8976;
 
 console.log(otherNumber.toPrecision(3));  // Give round off value till 3 digits.
+
+/*------------Maths -------------*/
+
+console.log(Math);
+console.log(Math.abs(-4));       //4
+console.log(Math.round(4.6));     //5
+console.log(Math.ceil(4.2));     //5
+console.log(Math.floor(4.7));      //4
+console.log(Math.min(4,3,6,8));    //3
+console.log(Math.max(4,3,6,8));    //8
+
+//Math.random
+console.log(Math.random());
+console.log(Math.round(Math.random()*10) + 1);
+
+//Formula for printing any random number between a maximum and minimum number.
+
+let min = 10;
+let max = 20;
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+ 
