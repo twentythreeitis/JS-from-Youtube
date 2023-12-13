62ee94963c2a16fa5452977ad38c31ca1826541b
Aryan Vashith <118191817+twentythreeitis@users.noreply.github.com>
2023-12-13 09:43:11 +0000

    Maps

--- 05_loops/06_map.js
+++ 05_loops/06_map.js
@@ -52,5 +52,35 @@ const myNum = [10,9,8,7,6,5,4,3,2,1];
 /*------------------------------------------------------------*/
 //(V) Using filter() method we cannot modify but only filter out given array
 
-const addTen = myNum.filter( (num) => {return (num+10)});
-console.log(addTen);
+// const addTen = myNum.filter( (num) => {return (num+10)});
+// console.log(addTen);
+
+/*------------------------------------------------------------*/
+//(VI) Using map()
+
+// const addTen = myNum.map( (num) => num + 10);
+// console.log(addTen);
+
+/* 
+[
+  20, 19, 18, 17, 16,
+  15, 14, 13, 12, 11
+]
+*/
+
+
+/*------------------------------------------------------------*/
+//Chaining of map()
+const newNum = myNum
+  .map((num) => num * 10 )
+  .map( (num) => num + 3)
+  .filter( (num) => num >= 40)
+
+console.log(newNum);
+
+/*
+[
+  103, 93, 83, 73, 63,
+   53, 43
+]
+*/
