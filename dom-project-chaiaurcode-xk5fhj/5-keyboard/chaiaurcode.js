ddf34b896bc426ff17ff58902d97ba89fed39e08
Aryan Vashith <vasappening0000@gmail.com>
2023-12-20 03:49:48 +0530

    Key strokes

--- dom-project-chaiaurcode-xk5fhj/5-keyboard/chaiaurcode.js
+++ dom-project-chaiaurcode-xk5fhj/5-keyboard/chaiaurcode.js
@@ -1,1 +1,23 @@
-console.log('Project 5');
+/*The purpose of the project is to find the keyboard key stroked in a table */
+/*We get these options in the 'e' object that comes along with addEventListener*/
+
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+        <div class="color">
+        <table>
+        <tr>
+          <th>Key</th>
+          <th>Code</th>
+        </tr>
+        <tr>
+          <td>${e.key === " "? "Space": e.key}</td>
+          <td>${e.code}</td>
+        </tr>
+      </table>
+        </div>
+    
+    `
+})
+
